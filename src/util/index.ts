75b301b12d3f14f85ed249d86b3e864398528972
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-09-22 18:26:49 +0630

    fix: development fail

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,10 @@
 import { Mode } from './../components/Introduction';
+const isBrowser = typeof window !== 'undefined';
 export function switchToDarkMode(mode: Mode) {
   document.querySelector('html').classList.toggle('dark');
-  localStorage.setItem('mode', mode);
+  if (isBrowser) localStorage.setItem('mode', mode);
 }
 export function getMode(): Mode {
+  if (!isBrowser) return;
   return localStorage.getItem('mode') as Mode;
 }
