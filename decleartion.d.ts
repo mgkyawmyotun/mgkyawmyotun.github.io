e0c6089c388b84000c5ea73ac68e9710e7105154
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-09-28 18:37:42 +0630

    feat: add project card

--- decleartion.d.ts
+++ decleartion.d.ts
@@ -1,1 +1,5 @@
 declare module '*.scss';
+
+declare module '*.png';
+
+declare module '*.gif';
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,29 @@
 import type { FC } from 'react';
 import React from 'react';
+import covid19gif from '../images/covid19.gif';
 import { project, project__card } from '../scss/project.mod.scss';
-type ProjectCardType = {};
-export const ProjectCard: FC = () => {
+type ProjectCardType = {
+  img: JSX.Element;
+  header_text: string;
+  footer_text: string;
+  tags: string[];
+};
+export const ProjectCard: FC<ProjectCardType> = ({
+  img,
+  header_text,
+  footer_text,
+  tags,
+}) => {
   return (
     <div className={project__card}>
-      <img src="" alt="" />
-      <h1></h1>
-      <h2></h2>
-      <div></div>
+      {img}
+      <h1>{header_text}</h1>
+      <h2>{footer_text}</h2>
+      <div>
+        {tags.map((tag) => (
+          <div>{tag}</div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -17,9 +32,27 @@ export const MyProject: FC = () => {
     <div className={project}>
       <h1>Here some of my projects</h1>
       <div>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          img={<img src={covid19gif} alt="Covid19Project" />}
+          header_text="Covid19 Tracker App"
+          footer_text="The purspose of app is to get the real time update of covid-19 data across the world.
+Visaulize data on Map / Graph/ Diagram and more.. "
+          tags={['JAVASCRIPT', 'NodeJs', 'JAVA', 'GOogle', 'DataScience']}
+        />
+        <ProjectCard
+          img={<img src={covid19gif} alt="Covid19Project" />}
+          header_text="Covid19 Tracker App"
+          footer_text="The purspose of app is to get the real time update of covid-19 data across the world.
+Visaulize data on Map / Graph/ Diagram and more.. "
+          tags={['JAVASCRIPT', 'NodeJs', 'JAVA', 'GOogle', 'DataScience']}
+        />
+        <ProjectCard
+          img={<img src={covid19gif} alt="Covid19Project" />}
+          header_text="Covid19 Tracker App"
+          footer_text="The purspose of app is to get the real time update of covid-19 data across the world.
+Visaulize data on Map / Graph/ Diagram and more.. "
+          tags={['JAVASCRIPT', 'NodeJs', 'JAVA', 'GOogle', 'DataScience']}
+        />
       </div>
     </div>
   );
